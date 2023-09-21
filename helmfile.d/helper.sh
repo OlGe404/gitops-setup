36d0aed415fa1240a82b21574450fac23ffe813c
OlGe <klardoch@gmail.com>
2023-09-21 16:30:57 +0200

    fix missing -f param in helper.sh

--- helmfile.d/helper.sh
+++ helmfile.d/helper.sh
@@ -5,5 +5,5 @@ ENV=$1
 for helmfile in apps/*/helmfile.yaml; do
     appName=$(echo $helmfile | cut -d '/' -f2)
     mkdir -p templated/$ENV/$appName
-    helmfile template -e $ENV --include-crds > templated/$ENV/$appName/all.yaml
+    helmfile template -e $ENV -f $helmfile --include-crds > templated/$ENV/$appName/all.yaml
 done
