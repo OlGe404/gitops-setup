5931f14f30890e039683a8f716c9efa178024ead
OlGe <klardoch@gmail.com>
2023-09-22 07:20:20 +0200

    use git generator instead of file generator for easier app creation

--- helmfile.d/helper.sh
+++ helmfile.d/helper.sh
@@ -2,9 +2,8 @@
 
 ENV=$1
 
-mkdir -p templated/$ENV
-
 for helmfile in apps/*/helmfile.yaml; do
     appName=$(echo $helmfile | cut -d '/' -f2)
-    helmfile template -e $ENV -f $helmfile --include-crds > templated/$ENV/$appName.yaml
+    mkdir -p templated/$ENV/$appName
+    helmfile template -e $ENV -f $helmfile --include-crds > templated/$ENV/$appName/all.yaml
 done
